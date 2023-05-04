43a24cdf7ad01b7dba6fa65149c270f87186339e
Faiyaz Raza <iamfaiyazraza1106@gmail.com>
2023-05-05 01:44:44 +0530

    backtracking -04-05-2023

---
+++ NQueenSolutions.java
@@ -0,0 +1,18 @@
+class NQueenSolutions {
+
+	static int solveNQueen(int n, int[][] board, int i) {
+		// base case
+		if (i == n) return 1;
+
+		
+
+	}
+
+	public static void main(String[] args) {
+		
+		int n = 4;
+		int[][] board = new int[n][n];
+
+
+	}
+}
